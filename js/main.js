8f0fb5954f6ff3549c3ef021f0e8aae40d72438e
anuj-nair <nairanuj29@gmail.com>
2020-08-11 21:11:46 +0530

    index page Modified

--- js/main.js
+++ js/main.js
@@ -1,111 +1,3 @@
-/* ..............................................
-   google_map js
-   ................................................. */
-
-// This example adds a marker to indicate the position of Bondi Beach in Sydney,
-// Australia.
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: { lat: 40.645037, lng: -73.880224 },
-    styles: [
-      {
-        elementType: 'geometry',
-        stylers: [{ color: '#fefefe' }]
-      },
-      {
-        elementType: 'labels.icon',
-        stylers: [{ visibility: 'off' }]
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#616161' }]
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [{ color: '#f5f5f5' }]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#bdbdbd' }]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{ color: '#eeeeee' }]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#757575' }]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{ color: '#e5e5e5' }]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#9e9e9e' }]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{ color: '#eee' }]
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#3d3523' }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{ color: '#eee' }]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#616161' }]
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#9e9e9e' }]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{ color: '#e5e5e5' }]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [{ color: '#000' }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{ color: '#c8d7d4' }]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{ color: '#b1a481' }]
-      }
-    ]
-  });
-
-  var image = 'img/pin.svg';
-  var beachMarker = new google.maps.Marker({
-    position: { lat: 40.712775, lng: -74.005973 },
-    map: map,
-    icon: image
-  });
-}
-
 (function ($) {
   "use strict";
 
@@ -237,4 +129,17 @@ function initMap() {
   });
 
 })(jQuery);
+
+$('.copyMail').click(function(e) {
+  e.preventDefault()
+  var copyText = $(this).attr('href');
+  document.addEventListener('copy', function(e) {
+     e.clipboardData.setData('text/plain', copyText);
+     e.preventDefault();
+  }, true);
+
+  document.execCommand('copy');  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied";
+});
 
--- js/products.js
+++ js/products.js
@@ -1,21 +1,19 @@
-$('#sub_products a').on('click', function (e) {
-	e.preventDefault();
-	$(this).tab('show');
-});
-console.log(window.location);
-var loc = window.location.hash;
 
-if (loc != '') {
-	if (loc == '#ROS') {
-		$('#AC_tab').removeClass('active');
-		$('#AC').removeClass('active show');
+var currLoc = window.location.hash;
+
+if (currLoc != '') {
+    $('#AC_tab').removeClass('active');
+    $('#AC').removeClass('active show');
+	if (currLoc == '#ROS') {
 		$('#ROS_tab').addClass('active');
 		$('#ROS').addClass('active show');
-	} else if (loc == '#RP') {
-		$('#AC_tab').removeClass('active');
-		$('#AC').removeClass('active show');g
+	} else if (currLoc == '#RP') {
 		$('#RP_tab').addClass('active');
 		$('#RP').addClass('active show');
-		window.location.href = window.location.href.slice(0, -loc.length);
 	}
-}
+} 
+
+$('#sub_products a').on('click', function (e) {
+	e.preventDefault();
+    $(this).tab('show');
+});
