071f13548154ee5f87cd9e0ae91ecdc98e955538
anuj-nair <nairanuj29@gmail.com>
2020-08-13 00:03:46 +0530

    footer simplified

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 
 var currLoc = window.location.hash;
 
-if (currLoc != '') {
+if (currLoc == '#ROS' || currLoc == '#RP') {
     $('#AC_tab').removeClass('active');
     $('#AC').removeClass('active show');
 	if (currLoc == '#ROS') {
@@ -11,7 +11,23 @@ if (currLoc != '') {
 		$('#RP_tab').addClass('active');
 		$('#RP').addClass('active show');
 	}
-} 
+}else(currLoc == "#win" || currLoc == "#port" || currLoc == "#cac" || currLoc == "vert"){
+	$('#split_tab').removeClass('active');
+    $('#split').removeClass('active show');
+	if (currLoc == '#win') {
+		$('#win_tab').addClass('active');
+		$('#win').addClass('active show');
+	} else if (currLoc == '#port') {
+		$('#port_tab').addClass('active');
+		$('#port').addClass('active show');
+	} else if (currLoc == '#cac') {
+		$('#cac_tab').addClass('active');
+		$('#cac').addClass('active show');
+	} else if (currLoc == '#vert') {
+		$('#vert_tab').addClass('active');
+		$('#vert').addClass('active show');
+	}
+}
 
 $('#sub_products a').on('click', function (e) {
 	e.preventDefault();
