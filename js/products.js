7c954b4221efca8ce06ebe2c53bab562ecd9bb30
anuj-nair <nairanuj29@gmail.com>
2020-09-05 17:38:22 +0530

    final page refining

--- js/products.js
+++ js/products.js
@@ -1,68 +1,63 @@
 $('.pop-s-img').magnificPopup({
-	type: 'image',
+    type: 'image',
 });
 
 $('.pop-m-img').magnificPopup({
-	delegate: 'a',
-	gallery: {
-		enabled: true,
-	},
-	type: 'image',
+    delegate: 'a',
+    gallery: {
+        enabled: true,
+    },
+    type: 'image',
 });
 if (window.innerWidth > 991) {
-	$('#header .logo h1').addClass('mtop-head');
-	$('.main-nav').addClass('mtop-nav');
-	$('a[href="#mobDescriptionContent"]').attr('href','#descriptionContent')
+    $('#header .logo h1').addClass('mtop-head');
+    $('.main-nav').addClass('mtop-nav');
 } else {
-	$('#header .logo h1').removeClass('mtop-head');
-	$('.main-nav').removeClass('mtop-nav');
-	$('.prod-pic h2').html($('.prod-deat h2').html());
-	$('.prod-deat h2').html('');
-	$('a[href="#descriptionContent"]').attr('href','#mobDescriptionContent')
+    $('#header .logo h1').removeClass('mtop-head');
+    $('.main-nav').removeClass('mtop-nav');
+    $('.prod-pic h2').html($('.prod-deat h2').html());
+    $('.prod-deat h2').html('');
 
 }
 window.onresize = () => {
-	if (window.innerWidth > 991) {
-		$('#header .logo h1').addClass('mtop-head');
-		$('.main-nav').addClass('mtop-nav');
-		if ($('.prod-pic h2').html() != '') {
-			$('.prod-deat h2').html($('.prod-pic h2').html());
-			$('.prod-pic h2').html('');
-		}
-		$('a[href="#mobDescriptionContent"]').attr('href','#descriptionContent')
-	} else {
-		$('#header .logo h1').removeClass('mtop-head');
-		$('.main-nav').removeClass('mtop-nav');
-		if ($('.prod-deat h2').html() != '') {
-			$('.prod-pic h2').html($('.prod-deat h2').html());
-			$('.prod-deat h2').html('');
-		}
-		$('a[href="#descriptionContent"]').attr('href','#mobDescriptionContent')
-	}
+    if (window.innerWidth > 991) {
+        $('#header .logo h1').addClass('mtop-head');
+        $('.main-nav').addClass('mtop-nav');
+        if ($('.prod-pic h2').html() != '') {
+            $('.prod-deat h2').html($('.prod-pic h2').html());
+            $('.prod-pic h2').html('');
+        }
+    } else {
+        $('#header .logo h1').removeClass('mtop-head');
+        $('.main-nav').removeClass('mtop-nav');
+        if ($('.prod-deat h2').html() != '') {
+            $('.prod-pic h2').html($('.prod-deat h2').html());
+            $('.prod-deat h2').html('');
+        }
+    }
 
-	if (768 > window.innerWidth) {
-		$('header .logo h1 span').html('KCS');
-		$('header .logo h1 span').css({
-			'font-family': "'Heebo', sans-serif",
-			'font-size': '36px',
-		});
-	} else {
-		$('header .logo h1 span').html('Krishna Cooling Solutions');
-		$('header .logo h1 span').css({
-			'font-family': "'Montserrat', sans-serif",
-			'font-weight': '900',
-			'letter-spacing': '1px',
-			'font-size': '30px',
-		});
-	}
+    if (768 > window.innerWidth) {
+        $('header .logo h1 span').html('KCS');
+        $('header .logo h1 span').css({
+            'font-family': "'Heebo', sans-serif",
+            'font-size': '36px',
+        });
+    } else {
+        $('header .logo h1 span').html('Krishna Cooling Solutions');
+        $('header .logo h1 span').css({
+            'font-family': "'Montserrat', sans-serif",
+            'font-weight': '900',
+            'letter-spacing': '1px',
+            'font-size': '30px',
+        });
+    }
 };
 var moreText = 'Read More',
-	lessText = 'Read Less',
-	moreButton = $('.readMore');
-moreButton.click(function () {
-	if ($(this).text() == lessText) {
-		$('html, body').animate({ scrollTop: '0' }, 'fast');
-	}
-	$(this).text($(this).text() == moreText ? lessText : moreText);
+    lessText = 'Read Less',
+    moreButton = $('.readMore');
+moreButton.click(function() {
+    if ($(this).text() == lessText) {
+        $('html, body').animate({ scrollTop: '0' }, 'fast');
+    }
+    $(this).text($(this).text() == moreText ? lessText : moreText);
 });
-
